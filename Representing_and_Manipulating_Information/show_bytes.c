448f1a5bd3351bc1c94c065b567e2b534b32e86d
Platelets <wenkanedward@outlook.com>
2021-11-16 19:30:27 +0800

    add equals

--- Representing_and_Manipulating_Information/show_bytes.c
+++ Representing_and_Manipulating_Information/show_bytes.c
@@ -20,8 +20,21 @@ void show_pointer(void *x){
     show_bytes((byte_pointer) &x,sizeof(void *));
 }
 
+void inplace_swap(int *x,int *y){
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+}
+
+int equals(int x,int y){
+    return !v(x ^ y);
+}
+
 int main(){
-    const char* s= "12345";
-    show_bytes((byte_pointer)s,strlen(s));
+    // const char* s= "abcdef";
+    // show_bytes((byte_pointer)s,strlen(s));
+    // int x = 0x87654321;
+    // printf("0X%.8x",x ^ 0xFFFFFF00);
+    printf("%d",equals(1,1));
     return 0;
 }
